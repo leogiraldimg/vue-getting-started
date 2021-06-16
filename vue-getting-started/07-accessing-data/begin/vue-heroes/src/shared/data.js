1586d13471c3b8235aeea73f90bd7513a2fd8dfa
Leonardo Giraldi <leogiraldimg@gmail.com>
2021-06-15 22:41:47 -0300

    ♻️ correcoes de lint

--- vue-getting-started/07-accessing-data/begin/vue-heroes/src/shared/data.js
+++ vue-getting-started/07-accessing-data/begin/vue-heroes/src/shared/data.js
@@ -5,7 +5,7 @@ import { inputDateFormat } from './constants';
 import { API } from './config';
 
 const getHeroes = async function() {
-  const response = await axios.get(`${ API }/heroes.json`);
+  const response = await axios.get(`${API}/heroes.json`);
   const heroes = response.data.map(h => {
     h.originDate = format(h.originDate, inputDateFormat);
     return h;
