055944efce675a1adfa86e702029c509973da511
Leonardo Giraldi <leogiraldimg@gmail.com>
2021-06-22 08:42:58 -0300

    ✨ navigating vue apps with routing v3

--- vue-getting-started/08-routing/begin/vue-heroes/src/router.js
+++ vue-getting-started/08-routing/begin/vue-heroes/src/router.js
@@ -5,6 +5,8 @@ import HeroDetail from './views/hero-detail.vue';
 
 Vue.use(Router);
 
+const parseProps = r => ({ id: parseInt(r.params.id) });
+
 export default new Router({
   mode: 'history',
   base: process.env.BASE_URL,
@@ -22,7 +24,7 @@ export default new Router({
       path: '/heroes/:id',
       name: 'hero-detail',
       component: HeroDetail,
-      props: true,
+      props: parseProps,
     },
     {
       path: '/about',
